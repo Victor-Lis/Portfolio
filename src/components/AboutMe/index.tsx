401543c75243cfa13d85f250f0f55a0a2adef7db
Victor-Lis <109773129+Victor-Lis@users.noreply.github.com>
2024-12-02 23:01:47 -0300

    feat: Adicionando Objetivos na seção AboutMe

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -28,26 +28,60 @@ export default function AboutMe() {
           Tecnologia
         </h1>
         <p className="max-[700px]:text-2xl text-tertiary text-4xl exo-2-regular text-justify w-6/12 max-[800px]:w-8/12 max-[400px]:w-10/12 min-w-48 max-sm:hidden">
-          Sempre fui um apaixonado assíduo por qualquer tecnologia e inovação, quando mais novo adorava jogar videogames e imaginar como era o processo para seu desenvolvimento
+          Sempre fui um apaixonado assíduo por qualquer tecnologia e inovação,
+          quando mais novo adorava jogar videogames e imaginar como era o
+          processo para seu desenvolvimento
         </p>
         <p className="max-[700px]:text-2xl text-tertiary text-4xl exo-2-regular text-justify w-6/12 max-[800px]:w-8/12 max-[400px]:w-10/12 min-w-48 mt-4">
-          Após minha entrada na Etec Prof. Carmine Biagio Tundisi, onde realizei meu curso técnico em Desenvolvimento de Sistemas de 2022 até 2024, vi que esse mundo era muito mais complexo, com isso ao invés de me assustar ou afastar, me apaixonei ainda mais, já tendo desenvolvido até aqui: {" "}
+          Após minha entrada na Etec Prof. Carmine Biagio Tundisi, onde realizei
+          meu curso técnico em Desenvolvimento de Sistemas durante{" "}
+          <a
+            target="_blank"
+            rel="noreferrer"
+            className="text-secondary font-normal underline"
+            href="https://github.com/Victor-Lis?tab=overview&from=2022-12-01&to=2022-12-31"
+          >
+            2022
+          </a>
+          ,{" "}
+          <a
+            target="_blank"
+            rel="noreferrer"
+            className="text-secondary font-normal underline"
+            href="https://github.com/Victor-Lis?tab=overview&from=2023-12-01&to=2023-12-31"
+          >
+            2023
+          </a>{" "}
+          e{" "}
+          <a
+            target="_blank"
+            rel="noreferrer"
+            className="text-secondary font-normal underline"
+            href="https://github.com/Victor-Lis?tab=overview&from=2024-12-01&to=2024-12-31"
+          >
+            2024
+          </a>
+          , vi que esse mundo da tecnologia era muito mais complexo, com isso ao
+          invés de me assustar ou me afastar, me apaixonei ainda mais, já tendo
+          desenvolvido até aqui:{" "}
           <a
             target="_blank"
             rel="noreferrer"
             className="text-secondary font-normal underline"
             href="https://github.com/Victor-Lis?tab=repositories&q=&type=&language=typescript,javascript,html,css,tsx"
           >
-            sites 
-          </a>, {" "}
+            sites
+          </a>
+          ,{" "}
           <a
             target="_blank"
             rel="noreferrer"
             className="text-secondary font-normal underline"
             href="https://github.com/Victor-Lis?tab=repositories&q=app"
           >
-            apps 
-          </a>, {" "}
+            apps
+          </a>
+          ,{" "}
           <a
             target="_blank"
             rel="noreferrer"
@@ -55,7 +89,8 @@ export default function AboutMe() {
             href="https://github.com/Victor-Lis?tab=repositories&q=&language=c%2B%2B"
           >
             sistemas embarcados
-          </a>, {" "}
+          </a>
+          ,{" "}
           <a
             target="_blank"
             rel="noreferrer"
@@ -63,10 +98,15 @@ export default function AboutMe() {
             href="https://github.com/Victor-Lis?tab=repositories&q=API"
           >
             APIs
-          </a>...
+          </a>
+          ...
         </p>
         <p className="max-[700px]:text-2xl text-tertiary text-4xl exo-2-regular text-justify w-6/12 max-[800px]:w-8/12 max-[400px]:w-10/12 min-w-48 mt-4 max-sm:hidden">
-        Mas minha paixão mesmo é para o lado que interage com o usúario, entender os pensamentos que passam por sua cabeça, quais padrões, cores e tipografias usar, portanto gosto mais do lado Front-End, mas não tenho nenhum problema em ler, estudar e desenvolver outras stacks (até fora da programação).
+          Mas minha paixão mesmo é para o lado que interage com o usúario,
+          entender os pensamentos que passam por sua cabeça, quais padrões,
+          cores e tipografias usar, portanto gosto mais do lado Front-End, mas
+          não tenho nenhum problema em ler, estudar e desenvolver outras stacks
+          (até fora da programação).
         </p>
       </Section>
       <Section aosData="fade-right">
@@ -98,9 +138,9 @@ export default function AboutMe() {
           >
             projetos
           </a>{" "}
-          que já havia construído tinham mais cara de TCC, ainda si acreditei na ideia junto dos meus
-          orientadores e integrantes do grupo, então intensifiquei os meus
-          estudos a respeito dos temas do trabalho e das{" "}
+          que já havia construído tinham mais cara de TCC, ainda si acreditei na
+          ideia junto dos meus orientadores e integrantes do grupo, então
+          intensifiquei os meus estudos a respeito dos temas do trabalho e das{" "}
           <a
             target="_blank"
             rel="noreferrer"
@@ -108,7 +148,8 @@ export default function AboutMe() {
             href="https://github.com/Victor-Lis/EcoPlay-Web"
           >
             tecnologias utilizadas
-          </a>.
+          </a>
+          .
         </p>
       </Section>
       <Section aosData="fade-left">
@@ -120,29 +161,56 @@ export default function AboutMe() {
           Mundo Acadêmico
         </h1>
         <p className="max-[700px]:text-2xl text-tertiary text-4xl exo-2-regular text-justify w-6/12 max-[800px]:w-8/12 max-[400px]:w-10/12 min-w-48 max-sm:hidden">
-          Após algumas experiências em escrever conteúdos utilizando a linguagem acadêmica e por consequência conhecer mais sobre esse mundo, tenho em meus pensamentos foco para uma carreira profissional voltada a tecnologia, mas também surgiu como um forte interesse da minha parte a evolução acadêmica.
+          Após algumas experiências em escrever conteúdos utilizando a linguagem
+          acadêmica e por consequência conhecer mais sobre esse mundo, tenho em
+          meus pensamentos foco para uma carreira profissional voltada a
+          tecnologia, mas também surgiu como um forte interesse da minha parte a
+          evolução acadêmica.
         </p>
         <p className="max-[700px]:text-2xl text-tertiary text-4xl exo-2-regular text-justify w-6/12 max-[800px]:w-8/12 max-[400px]:w-10/12 min-w-48 mt-4">
-          É perceptivel a evolução da habilidade e prática do grupo falando de mundo acadêmico quando observamos a diferença entre as produções para {" "}
+          É perceptivel a evolução da habilidade e prática do grupo falando de
+          mundo acadêmico quando observamos a diferença entre as produções para{" "}
           <a
             target="_blank"
             rel="noreferrer"
             className="text-secondary font-normal underline"
             href="https://drive.google.com/file/d/1d5dw6X9UM-dh-K3sY_HXURzOot5TlC5z/view?usp=drive_link"
           >
-            Feteps 2024 
-          </a>
-          {" "}(que o ocorreu entre 2023 e 2024) e a produção do {" "}
+            Feteps 2024
+          </a>{" "}
+          (que o ocorreu entre 2023 e 2024) e a produção do{" "}
           <a
             target="_blank"
             rel="noreferrer"
             className="text-secondary font-normal underline"
             href="https://www.linkedin.com/posts/victor-lis-bronzo_eco-play-activity-7266495833804558336-cO3f?utm_source=share&utm_medium=member_desktop"
           >
-          artigo do TCC 
+            artigo do TCC
           </a>{" "}
           (produzido ao longo de 2024).
         </p>
+      </Section>
+      <Section aosData="fade-right">
+        <h1
+          className={
+            "max-[700px]:text-6xl uppercase text-secondary font-bold text-8xl bebas-neue w-6/12 max-[800px]:w-8/12 max-[400px]:w-10/12 min-w-48"
+          }
+        >
+          Objetivos
+        </h1>
+        <p className="max-[700px]:text-2xl text-tertiary text-4xl exo-2-regular text-justify w-6/12 max-[800px]:w-8/12 max-[400px]:w-10/12 min-w-48">
+          Tenho como meus objetivos para essa nova etapa:
+        </p>
+        <ul className="max-[700px]:text-2xl text-tertiary text-4xl exo-2-regular text-justify w-6/12 max-[800px]:w-8/12 max-[400px]:w-10/12 min-w-48 list-disc marker:text-secondary">
+          <li className="w-11/12 ml-auto">
+            Ingressar em um curso tecnólogo no próximo ano, já que ainda sou muito novo e até
+            aqui o tempo sempre jogou a meu favor
+          </li>
+          <li className="w-11/12 ml-auto">
+            Para buscar experiência e consolidação dos meus aprendizados pretendo o quanto antes ingressar no
+            meio profissional.
+          </li>
+        </ul>
       </Section>
     </div>
   );
