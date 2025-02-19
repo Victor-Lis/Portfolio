b0c1954d6804a3ba02739e8fa230f47b5bc24582
Victor-Lis <109773129+Victor-Lis@users.noreply.github.com>
2025-02-19 18:25:12 -0300

    fix: Concertando layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,11 +20,11 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
