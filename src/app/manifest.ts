aaa1726ef116b0bfdfd80d5ca86597181221c66e
Victor-Lis <109773129+Victor-Lis@users.noreply.github.com>
2025-04-19 16:24:16 -0300

    fix: update manifest.ts colors

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -7,8 +7,8 @@ export default function manifest(): MetadataRoute.Manifest {
     description: 'Portfolio showcasing Victor Lis Bronzo, a Web/Full-Stack Developer from Brazil.',
     start_url: '/',
     display: 'standalone',
-    background_color: '#1e40af',
-    theme_color: '#1e40af',
+    background_color: '#2a2c41',
+    theme_color: '#34df8b',
     icons: [
       {
         src: '/favicon.ico',
